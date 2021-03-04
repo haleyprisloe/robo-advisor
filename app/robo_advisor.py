318e4773b514cfee026c5221122bd747022a1471
haleyprisloe <77749998+haleyprisloe@users.noreply.github.com>
2021-03-04 14:52:12 -0500

    latest day

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,14 +7,34 @@ import json
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 response = requests.get(request_url)
-print(type(response)) #requests.models.Response
-print(response.status_code) #200
-print(response.text)
+#print(type(response)) #requests.models.Response
+#print(response.status_code) #200
+#print(response.text)
 
+# parse response text to a dictionary
+parsed_response = json.loads(response.text)
 
-quit()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+#breakpoint()
+
 
 
 # INFO OUTPUTS 
 
-
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
